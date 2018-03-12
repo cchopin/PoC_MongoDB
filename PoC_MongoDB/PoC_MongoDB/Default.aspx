d4497e5b1cc91a56efa1816b52194b221379f253
cchopin <cchopin.externe@altima-assurances.fr>
2018-03-12 21:33:10 +0100

    test select

--- PoC_MongoDB/PoC_MongoDB/Default.aspx
+++ PoC_MongoDB/PoC_MongoDB/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PoC_MongoDB.Default" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PoC_MongoDB.Default" Async="true" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
@@ -28,12 +28,24 @@
                     <span class="label-input100">Objet à assurer *</span>
                     <div>
 
-                        <asp:DropDownList ID="ddlcategorie" runat="server" class="js-select2" name="service">
+                        <asp:DropDownList ID="ddlcategorie" runat="server" class="js-select2" name="service" AutoPostBack="true" OnSelectedIndexChanged="ddlcategorie_SelectedIndexChanged">
                         </asp:DropDownList>
 
                         <div class="dropDownSelect2"></div>
                     </div>
                 </div>
+
+                  <div class="wrap-input100 input100-select bg1">
+                    <span class="label-input100">valeur</span>
+                    <div>
+
+                        <asp:TextBox ID="txtValeur" runat="server"  TextMode="MultiLine" Width="100%"/>
+
+                        <div class="dropDownSelect2"></div>
+                    </div>
+                </div>
+
+
                 <div class="container-contact100-form-btn">
                     <button class="contact100-form-btn">
                         <span>Envoyer
--- PoC_MongoDB/PoC_MongoDB/Default.aspx.cs
+++ PoC_MongoDB/PoC_MongoDB/Default.aspx.cs
@@ -7,20 +7,46 @@ namespace PoC_MongoDB
 {
     public partial class Default : System.Web.UI.Page
     {
+        static IMongoClient client = new MongoClient("mongodb://localhost:27017/");
+        static IMongoDatabase database = client.GetDatabase("cbien");
+
         protected void Page_Load(object sender, EventArgs e)
-        {
-
-            IMongoClient client = new MongoClient("mongodb://localhost:27017/");
-            IMongoDatabase database = client.GetDatabase("cbien");
+        {            
             IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("tarifs");
-            
-            
-
-            var distinctAttributeNames = collection.Distinct(new StringFieldDefinition<BsonDocument, string>("categorie"), new BsonDocument()).ToList();
+          
+            var distinctAttributeNames = collection.Distinct(new StringFieldDefinition<BsonDocument, string>("Categorie"), new BsonDocument()).ToList();
 
             foreach (var distinctItem in distinctAttributeNames)
             {
                 ddlcategorie.Items.Add(distinctItem.ToString());
+            }
+        }
+
+        protected async void ddlcategorie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("tarifs");
+
+            BsonDocument filter = new BsonDocument();
+            BsonDocument projection = new BsonDocument();
+            projection.Add("SousCategorie.GarantieTarif.GarantieCode", 1);
+
+            var options = new FindOptions<BsonDocument>()
+            {
+                Projection = projection
+            };
+
+            using (var cursor = await collection.FindAsync(filter, options))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    var batch = cursor.Current;
+                    int i = 0;
+                    foreach (BsonDocument document in batch)
+                    {
+                        txtValeur.Text += document[1][0][0][i][0].AsBsonValue.ToString() + " ";
+                        i++;
+                    }
+                }
             }
         }
     }
